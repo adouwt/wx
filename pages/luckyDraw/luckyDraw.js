ca6def10c53786780496bc171af20fbe84aaec41
wnagtao <1259709654@qq.com>
2019-02-11 22:37:44 +0800

    修改抽奖页面

--- pages/luckyDraw/luckyDraw.js
+++ pages/luckyDraw/luckyDraw.js
@@ -1,9 +1,27 @@
-//logs.js
-const util = require('../../utils/util.js')
-
+//index.js
+//获取应用实例
+const app = getApp();
+const network = require("../../utils/api.js");
 Page({
   data: {
+    wxAllUser: []
   },
   onLoad: function () {
+    this.initData()
+  },
+  initData: function(){
+    let _this = this
+    network.getData('/get/alluser', 
+    {},
+    '', 
+    function (res) {
+      _this.setData({
+        wxAllUser: res.users
+      })
+    }, function () {
+      wx.showToast({
+        title: '获取签到数据失败',
+      })
+    })
   }
 })
