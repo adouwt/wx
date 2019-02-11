e3b86761318fa236212428470cbee3aa92329213
wnagtao <1259709654@qq.com>
2019-02-11 21:17:14 +0800

    【20190211】修改签到，增加抽奖页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,8 +11,10 @@ Page({
     primarySize: 'default',
     warnSize: 'default',
     disabled: true,
-    plain: false,
-    loading: false
+    isSignIn: false,
+    loading: false,
+    currentIndex: 0,
+    allIndex: 0
   },
   //事件处理函数
   bindViewTap: function() {
@@ -27,6 +29,7 @@ Page({
         hasUserInfo: true,
         disabled: false
       })
+      this.initData()
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -36,6 +39,7 @@ Page({
           hasUserInfo: true,
           disabled: false
         })
+        this.initData()
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
@@ -70,6 +74,11 @@ Page({
         icon: 'fail',
         duration: 1000
       })
+      _this.setData({
+        isSignIn: true,
+        currentIndex: res.currentIndex || 0,
+        allIndex: res.allIndex || 0
+      })
     }, function () {
       wx.showToast({
         title: '加载数据失败',
@@ -85,4 +94,24 @@ Page({
       console.log(res, 'get')
     })
   },
+  initData: function(){
+    let _this = this
+    network.postData('/post/wexinSignInInit', 
+    _this.data.userInfo, 
+    '', 
+    function (res) {
+      console.log(res)
+      if (res.isSignIn) {
+        _this.setData({
+          isSignIn: true,
+          currentIndex: res.currentIndex || 0,
+          allIndex: res.allIndex || 0
+        })
+      }
+    }, function () {
+      wx.showToast({
+        title: '签到初始化失败',
+      })
+    })
+  }
 })
